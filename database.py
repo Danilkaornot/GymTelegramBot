07ed772dca85cb9a4dbd83e0f7dd1ff4b75dc46c
Danilkaornot <Turcey432@gmail.com>
2024-12-08 13:23:46 +0300

    init commit

--- database.py
+++ database.py
@@ -4,11 +4,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
-
+from models import User, Workout, Cardio, Exercises
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+
+
+def create_db_and_tables() -> None:
+    Base.metadata.create_all(engine)
+
+Session = sessionmaker(engine)
+#with Session() as session:
+
--- models.py
+++ models.py
@@ -1,14 +1,41 @@
 
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from database import Base
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
+from sqlalchemy.orm import relationship, DeclarativeBase
+
+class Base(DeclarativeBase):
+	pass
 
 class User(Base):
     __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    username=Column(String, nullable=False)
+    telegramid=Column(Integer, nullable=False)
 
-    # TODO: Распипсать поля 
+class Approach:
+    id = Column(Integer, primary_key=True, index=True)
+    telegramid = Column(Integer, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    exercise_id = Column(String, nullable=False)
 
-class Workout(Base):
+
+
+class Workout(Base, Approach):
     __tablename__ = 'workouts'
+    weight = Column(Integer)
+    count = Column(Integer, nullable=False)
 
-    # TODO: Распипсать поля 
+class Cardio(Base, Approach):
+    __tablename__ = 'cardio'
+    distance = Column(Integer, nullable=False)
+    time = Column(Integer, nullable=False)
+
+class Exercises(Base):
+    __tablename__ = 'exercises'
+    id = Column(Integer, primary_key=True, index=True)
+    telegramid = Column(Integer, nullable=False)
+    weighting = Column(Boolean, nullable=False)
+    counting = Column(Boolean, nullable=False)
+    timing = Column(Boolean,nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    exercise_name = Column(String, nullable=False)
+
